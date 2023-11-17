0163b05a1819f72aef65596b9833e6036dabfcff
miyan <1270308266@qq.com>
2023-11-17 19:40:22 +0800

    添加组件

--- myspace/src/views/HomeView.vue
+++ myspace/src/views/HomeView.vue
@@ -26,6 +26,18 @@
           </div>
         </div>
       </div>
+      <div class="col-sm-3 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body" style="width: 18rem;">
+            <img src="https://www.notion.so/images/page-cover/woodcuts_3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">SuperCook</h5>
+              <p class="card-text">SuperCook是一款免费在线食谱生成器/食谱搜索引擎，可让你根据家里现有的食材进行搜索，输入食材自动出菜谱，帮助用户找到数以千计的食谱，充分利用家里的食材，非常方便快捷的在线食谱生成器和食谱搜索引擎，而且支持全球多种语言，使用无需注册登录，还支持手机端app</p>
+              <a href="https://www.miyan.ac.cn/" target="_blank" class="btn btn-primary">打开</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </ContentBase>
@@ -46,6 +58,17 @@ export default {
 </script>
 
 <style>
+.card-img-top {
+  width: 256px;
+  height: 170px;
+}
+.card-text {
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 .container {
   margin-top: 20px;
 }
